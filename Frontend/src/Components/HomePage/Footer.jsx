e59027669eda67c5c1d4c879fdab8427b1e1ab29
tiagodccosta <tiagocunhacosta@gmail.com>
2024-11-05 13:00:53 +0000

    Finnishing Translations

--- Frontend/src/Components/HomePage/Footer.jsx
+++ Frontend/src/Components/HomePage/Footer.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import BloodFlowLogo from "../../Assets/BloodflowLogo.png";
+import { useTranslation } from 'react-i18next';
 
 
 const Footer = () => {
+
+    const { t } = useTranslation();
+
     return (
         <div className="bg-black w-full h-full flex flex-col items-center text-center text-white pt-16">
             <h3 className="text-2xl sm:text-4xl font-bold mb-4 max-w-[650px]">
-                Ready to transform your blood test analysis with AI?
+                {t('ReadyToTransform')}
             </h3>
             <p className="text-sm sm:text-lg text-gray-400 mb-8 max-w-[650px] px-6">
-                Contact us today to schedule a demo and see how our AI-driven solution can streamline your clinic’s blood test analysis process.
+                {t('ReadyToTransform2')}
             </p>
             <a 
                 href="https://calendly.com/tiago-costa-bloodflow" 
@@ -17,7 +21,7 @@ const Footer = () => {
                 rel="noopener noreferrer" 
                 className="bg-[#ff0000] hover:bg-red-600 text-white font-semibold py-3 px-6 rounded-full transition-colors duration-300"
             >
-                Book a call with us
+                {t('BookDemo')}
             </a>
             
             <div className="bg-gray-900 w-full flex items-center justify-between text-left mt-36 px-6 md:px-12 lg:px-24 py-16">
@@ -25,9 +29,9 @@ const Footer = () => {
 
                 <div className="text-white-400 mt-6 md:mt-0">
                     <p className="font-semibold">BloodFlow</p>
-                    <p>Rua da Prata 80</p>
-                    <p>Lisbon, Portugal</p>
-                    <p className="mt-4">info@bloodflow.pt</p>
+                    <p>{t('footerAddress')}</p>
+                    <p>{t('footerAddress2')}</p>
+                    <p className="mt-4">{t('footerEmail')}</p>
                 </div>
             </div>
 
