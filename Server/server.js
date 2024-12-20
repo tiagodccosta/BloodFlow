51cc156e7e2799be22226d669abe9deb1c07b225
tiagodccosta <tiagocunhacosta@gmail.com>
2024-12-20 12:10:05 +0000

    Fixes to endpoint

--- Server/server.js
+++ Server/server.js
@@ -1015,7 +1015,6 @@ app.post('/ai-agent/analyse', async (req, res) => {
     }
 });
 
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
